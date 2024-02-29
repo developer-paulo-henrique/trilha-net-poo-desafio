c76f08e13a7f2a528fad93dfb3f504459b396844
Paulo Henrique <paulo.henrique@ebitstec.com.br>
2024-02-29 10:26:08 -0300

    Conclusão do desafio 2902024

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartfone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "11111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+nokia.ReceberLigacao();
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartfone Iphone:");
+Smartphone iphone = new Iphone("4987", "Modelo 2", "22222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
